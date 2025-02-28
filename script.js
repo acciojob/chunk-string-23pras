3674d39b04d2a8baa6f54cce290353894011c342
Prashant Pal <palprashant465@gmail.com>
2025-02-28 19:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
+function stringChop(str, n) {
+    if (str === null) return [];
+    let arr = [];
+    n = parseInt(n, 10); // Convert chunk size to an integer
 
-function stringChop(str,n){
-    let arr = []
-    if(str.length<n) {
-     return arr
+    for (let i = 0; i < str.length; i += n) {
+        let val = str.substring(i, i + n);
+        arr.push(val);
     }
-    else if(str.length>n){
-      for(let i=0; i<str.length; i=i+n){
-          let val = str.substring(i,i+n);
-          arr.push(val);
-      }  
-      return arr
-    }
-    return arr
+
+    return arr;
 }
 
 // Do not change the code below
+
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
-
-
-
